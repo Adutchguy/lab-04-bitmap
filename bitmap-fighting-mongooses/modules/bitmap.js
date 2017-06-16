e27cd67445f504f2452b9e8ae9457c48a0947d2b
Kyle Aardal <aardal.kyle@gmail.com>
2017-06-15 18:18:27 -0700

    fixed Math.floor on pixelRowSize

--- bitmap-fighting-mongooses/modules/bitmap.js
+++ bitmap-fighting-mongooses/modules/bitmap.js
@@ -12,7 +12,7 @@ bitmap.Bitmap = function (buffer) {
   this.fileSize = buffer.readUInt32LE(2);
   //gets the number of colors in the table, multiplys it by 4 (number of bytes per color), and adds to offset
   this.colorTable = buffer.slice(54, (buffer.readUInt32LE(46) * 4) + 54).toString('hex');
-  this.pixelRowSize = Math.floor(((buffer.readUInt16LE(28) * this.width + 31) / 32) * 4);
+  this.pixelRowSize = Math.floor(((buffer.readUInt16LE(28) * this.width + 31) / 32)) * 4;
   this.pixelArraySize = this.pixelRowSize * Math.abs(this.height);
   this.pixelArray = buffer.slice((buffer.readUInt32LE() * 4) + 54), this.pixelArraySize + ((buffer.readUInt32LE() * 4) + 54);
 };
