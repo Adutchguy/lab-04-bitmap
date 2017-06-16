60370ee17b9da45b7c29c2fb37a2d8ac43535142
Joshua Evans <skyfriends1@gmail.com>
2017-06-16 15:20:03 -0700

    added all three methods, tested and working, going to start working on  test files now. also needs some refactoring to look purttty

---
+++ bitmap-fighting-mongooses/tests/bitmap-test.js
@@ -0,0 +1,5 @@
+'use strict';
+
+describe('testing bitmap module', () => {
+  it('should', () => {});
+});
---
+++ bitmap-fighting-mongooses/tests/index-test.js
@@ -0,0 +1,5 @@
+'use strict';
+
+describe('testing index', () => {
+  it('should', () => {});
+});
